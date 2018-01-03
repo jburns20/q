630f0c1dac357e86c6066eb9926ba61a091d45b6
Jonathan Burns <j@burnsfamily.info>
2018-01-03 15:56:55 -0800

    Refactor homepage to use promises

--- routes/options.js
+++ routes/options.js
@@ -7,60 +7,58 @@ var allowed_tags = "<a><b><blockquote><code><del><dd><dl><dt><em><h1><h2><h3><h4
 
 var frozen_cache = null;
 
-exports.frozen = function(callback) {
-    if (frozen_cache === null) {
-        return model.Option.findOne({
-            where: {key: "frozen"}
-        }).then(function(row) {
-            if (row && row.value == "1") {
-                frozen_cache = true;
-                return callback(true);
-            } else if (row && row.value == "0") {
-                frozen_cache = false;
-                return callback(false);
-            } else {
-                return model.Option.create({
-                    key: "frozen",
-                    value: "0"
-                }).then(function() {
-                    frozen_cache = false;
-                    return callback(false);
-                });
-            }
-        });
-    } else {
-        return callback(frozen_cache);
+exports.frozen = function() {
+    if (frozen_cache !== null) {
+        return Promise.resolve(frozen_cache);
     }
+    return model.Option.findOne({
+        where: {key: "frozen"}
+    }).then(function(row) {
+        if (row && row.value == "1") {
+            frozen_cache = true;
+            return true;
+        } else if (row && row.value == "0") {
+            frozen_cache = false;
+            return false;
+        } else {
+            return model.Option.create({
+                key: "frozen",
+                value: "0"
+            }).then(function() {
+                frozen_cache = false;
+                return false;
+            });
+        }
+    });
 };
 
 var message_cache = null;
 
-exports.message = function(callback) {
-    if (message_cache === null) {
-        return model.Option.findOne({
-            where: {key: "message"}
-        }).then(function(row) {
-            if (row) {
-                message_cache = row.value;
-                return callback(row.value);
-            } else {
-                return model.Option.create({
-                    key: "message",
-                    value: ""
-                }).then(function() {
-                    message_cache = "";
-                    return callback("");
-                });
-            }
-        });
-    } else {
-        return callback(message_cache);
+exports.message = function() {
+    if (message_cache !== null) {
+        return Promise.resolve(message_cache);
     }
+    return model.Option.findOne({
+        where: {key: "message"}
+    }).then(function(row) {
+        if (row) {
+            message_cache = row.value;
+            return row.value;
+        } else {
+            return model.Option.create({
+                key: "message",
+                value: ""
+            }).then(function() {
+                message_cache = "";
+                return "";
+            });
+        }
+    });
 };
 
 exports.get = function(req, res) {
     if (req.query.key == "frozen") {
-        exports.frozen(function(frozen) {
+        exports.frozen().then(function(frozen) {
             if (frozen) {
                 res.send("1");
             } else {
@@ -93,10 +91,10 @@ exports.post = function(req, res) {
         return;
     }
     var promises = [];
-    
+
     if (req.body.frozen == "0" || req.body.frozen == "1") {
         var prev_frozen = null;
-        var p = exports.frozen(function(result) {
+        var p = exports.frozen().then(function(result) {
             prev_frozen = result;
             return model.Option.upsert({
                 key: "frozen",
@@ -106,17 +104,17 @@ exports.post = function(req, res) {
             frozen_cache = null;
             if (req.body.frozen == "1" && !prev_frozen) {
                 realtime.frozen(true);
-                return Promise.resolve("Queue frozen");
+                return "Queue frozen";
             } else if (req.body.frozen == "0" && prev_frozen) {
                 realtime.frozen(false);
-                return Promise.resolve("Queue unfrozen");
+                return "Queue unfrozen";
             } else {
-                return Promise.resolve(null);
+                return null;
             }
         });
         promises.push(p);
     }
-    
+
     if (req.body.message || req.body.message === "") {
         var message = striptags(req.body.message, allowed_tags);
         var p = model.Option.upsert({
@@ -126,25 +124,15 @@ exports.post = function(req, res) {
             message_cache = null;
             realtime.message(message);
             if (message == "") {
-                return Promise.resolve("Message removed");
+                return "Message removed";
             } else {
-                return Promise.resolve("Message updated");
+                return "Message updated";
             }
         });
         promises.push(p);
     }
-    
+
     Promise.all(promises).then(function(results) {
-        var message = "";
-        results.forEach(function(result) {
-            if (result != null) {
-                if (message == "") {
-                    message += "" + result;
-                } else {
-                    message += ", " + result;
-                }
-            }
-        });
-        respond(req, res, message);
+        respond(req, res, results.filter(function(x) {return x != null}).join(', '));
     });
 };
